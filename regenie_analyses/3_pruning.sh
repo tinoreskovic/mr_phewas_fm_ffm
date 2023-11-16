dd77fb6f4d72aa90135f22c2ea89e282caaf7bc2
Tin Oreskovic <tin_oreskovic@alumni.brown.edu>
2023-11-16 18:24:15 +0000

    Update 3_pruning.sh
    correction

--- regenie_analyses/3_pruning.sh
+++ regenie_analyses/3_pruning.sh
@@ -12,6 +12,6 @@ wc *.bim "
 dx run swiss-army-knife -iin="${data_file_dir}/${data_field}_allQC_v2_merged_phe.bed" \
    -iin="${data_file_dir}/${data_field}_allQC_v2_merged_phe.bim" \
    -iin="${data_file_dir}/${data_field}_allQC_v2_merged_phe.fam"\
-   -iin="${txt_file_dir}/fm_ffm_4regenie_wes.phe" \
+   -iin="${txt_file_dir}/fm_ffm_4regenie.phe" \
    -icmd="${run_plink_qc}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
    --destination="${project}:/data/output_path_2/" --brief --yes
